27456a6a315bd1cda1ebfefce8bea46f6dc23683
“Ellie <epapp1@lsu.edu>
2024-09-23 09:29:19 -0500

    save 3

--- sketch.js
+++ sketch.js
@@ -4,11 +4,48 @@ function setup(){
 }
 function draw(){
 
-    var squareSize = 175
-    stroke(1000);
-    for(var i = squareSize/1.25; i < 500; i = 5 =
-    ){
-        rect(i, 40, i + 60, 80);
+    rotate
+    strokeWeight(5);
+    for(var m =0; m < height+1; m++){
+        fill(0);
+        line(0, m, width, m);
+        stroke(m/1.5, 225, 145);
+    }
+    
+    fill(235, 252, 235);
+    ellipse(380, 520, 500);
+    fill(235, 252, 235);
+    
+
+    for(var y = -25; y <= height+30; y += 10){
+        for(var x = -500; x <= width; x += 30){
+            strokeWeight(1);
+            fill(237, 202, 252);
+            stroke(245, 227, 252);
+            rect(x+y , y, 17- y/20, 17- y/20);
+
+            fill(0);
+            
+        }
+    }
+
+
+    for(var a = -200; a <= 500; a += 60){
+        for(var b = 10; b <= height;  b += 75){
+            strokeWeight(2);
+            stroke(240, 209, 137);
+            fill(240, 193, 84);
+            rect(a-(b*1), 500-(b/2), 50-(a/10), 50+(a/10));
+        }
+    }
+
+    for(var a = 200; a <= 500; a += 60){
+        for(var b = -10; b <= width;  b += 75){
+            strokeWeight(2);
+            stroke(240, 209, 137);
+            fill(240, 193, 84);
+            rect(a-(b*1.), 0-(b/2), 25-(a/10), 100+(a/10));
+        }
     }
 }
 
