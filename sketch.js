dae71f6ccdf01c01d7fa8b6cf2e17b5dd92fff34
“Ellie <epapp1@lsu.edu>
2024-09-23 07:15:19 -0500

    save 2

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-function setup() {
+function setup(){
     createCanvas(500, 500);
-    background(137, 96, 145);
+    background(137,96, 145);
+}
+function draw(){
 
     var squareSize = 175
-
-    for(var i = squareSize/1.25; i <= height; i -= 5){
-        
+    stroke(1000);
+    for(var i = squareSize/1.25; i < 500; i = 5 =
+    ){
+        rect(i, 40, i + 60, 80);
     }
-
-
-
 }
+
+    
