55baea2ff15220a408d13330d02e2af0a3aff130
“Ellie <epapp1@lsu.edu>
2024-09-23 06:54:02 -0500

    save 1

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(500, 500);
+    background(137, 96, 145);
+
+    var squareSize = 175
+
+    for(var i = squareSize/1.25; i <= height; i -= 5){
+        
+    }
+
+
+
+}
